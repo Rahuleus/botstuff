0df0b97ff86661f4ec6220acc99ba04d615b5775
Rahuleus <124668984+Rahuleus@users.noreply.github.com>
2023-08-26 13:30:45 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,31 @@
+import os
+import discord
+import requests
+import json
+import random
+ 
+intents = discord.Intents().all()
+client = discord.Client(intents=intents)
+ 
+ 
+#first event :logging in
+@client.event
+async def on_ready():
+  print("successful login as {0.user}".format(client))
+ 
+ 
+#second event: sending message
+@client.event
+async def on_message(message):
+  #check who sent the message
+  if message.author == client.user:
+    return
+  msg = message.content
+  if msg.startswith('hello'):
+    await message.channel.send("hello!")
+ 
+ 
+#getting the secret token
+f = open("demofile", "r")
+n=(f.read())
+client.run(n)
