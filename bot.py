7f0c535726ff092284f0db28bc55ce7dba027078
Rahuleus <124668984+Rahuleus@users.noreply.github.com>
2023-08-28 19:21:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,12 +22,16 @@ async def on_message(message):
   msg = message.content
   if msg.startswith('hello'):
     await message.channel.send("hello!")
- 
+    print('hello')
+  elif msg.startswith('meme'):
+    await message.channel.send("https://media.tenor.com/yIWM3TO6ScEAAAAd/meme-approved-knuckles.gif")
+    print("meme")
  
 #getting the secret token
 f = open("demofile", "r")
 n=(f.read())
 a=""
-for i in range(len(n)-2):
+for i in range(len(n)-1):
     a += n[i]
 client.run(a)
+
